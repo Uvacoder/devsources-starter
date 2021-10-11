91c25cafbd228bfef3ee6a51d8d7d0056fc04048
codetann <thetannerthomas@gmail.com>
2021-10-10 22:50:28 -0600

    refactor navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,31 +6,26 @@ import {
   Link as CLink,
 } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
-import { BsHouse } from "react-icons/bs";
-
-const Links = [{ path: "/", name: "Home", icon: <BsHouse /> }];
+import { BsList } from "react-icons/bs";
 
 export const Navbar = () => {
   return (
-    <HStack w="100%" maxW="6xl" pos="fixed" top="0" p="1rem 0" spacing="2rem">
+    <HStack
+      w="100%"
+      maxW="6xl"
+      pos="fixed"
+      top="0"
+      py="1rem"
+      px="2rem"
+      spacing="2rem"
+    >
+      {/* title */}
       <Heading fontSize="2xl" color="#0a2540">
         .sources
       </Heading>
       <Spacer />
 
-      <HStack spacing="1rem">
-        {Links.map((link, i) => (
-          <CLink key={i} _hover={{ opacity: 0.6 }}>
-            <Link style={{ display: "flex", alignItems: "center" }}>
-              {link.icon} {link.name}
-            </Link>
-          </CLink>
-        ))}
-      </HStack>
-      <HStack spacing="1rem">
-        <IconButton icon={<p>❤️</p>} />
-        <IconButton icon={<p>🌜</p>} />
-      </HStack>
+      <IconButton bg="none" fontSize="24px" icon={<BsList />} />
     </HStack>
   );
 };
